51e11c7041372f2179a3e6258f6ab73ed46e475e
Savyam Shukla <savyamshukla@gmail.com>
2026-02-08 10:18:45 +0530

    adminflowClean1

--- src/main/java/com/notes/notesplatform/repository/CourseRepository.java
+++ src/main/java/com/notes/notesplatform/repository/CourseRepository.java
@@ -1,4 +1,4 @@
-package com.notes.notesplatform.repository;
+/*package com.notes.notesplatform.repository;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,4 +11,21 @@ public interface CourseRepository extends JpaRepository<Course, Long> {
      List<Course> findByDeletedTrue();
      
      
+}*/
+
+package com.notes.notesplatform.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.notes.notesplatform.model.Course;
+
+public interface CourseRepository extends JpaRepository<Course, Long> {
+
+    // Adding both subjects and classes to the attributePaths
+    @EntityGraph(attributePaths = {"subjects", "classes"})
+    List<Course> findByDeletedFalse();
+
+    @EntityGraph(attributePaths = {"subjects", "classes"})
+    List<Course> findByDeletedTrue();
 }
