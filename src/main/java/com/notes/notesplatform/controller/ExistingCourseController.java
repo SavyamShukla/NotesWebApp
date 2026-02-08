ba3b6046c1b7c51fc814c1e8ff89fc5292dc7421
Savyam Shukla <savyamshukla@gmail.com>
2026-02-08 16:31:40 +0530

    wsxc

--- src/main/java/com/notes/notesplatform/controller/ExistingCourseController.java
+++ src/main/java/com/notes/notesplatform/controller/ExistingCourseController.java
@@ -182,12 +182,13 @@ import com.notes.notesplatform.repository.*;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
 
-@RestController
+@Controller
 @RequestMapping("/existing-courses")
 public class ExistingCourseController {
 
