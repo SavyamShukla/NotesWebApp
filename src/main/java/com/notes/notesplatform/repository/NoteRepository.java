31558bb789437f9bf8a8ca69b8e1847d7b1a33fb
Savyam Shukla <savyamshukla@gmail.com>
2026-02-08 19:53:18 +0530

    ygf

--- src/main/java/com/notes/notesplatform/repository/NoteRepository.java
+++ src/main/java/com/notes/notesplatform/repository/NoteRepository.java
@@ -1,9 +1,12 @@
 package com.notes.notesplatform.repository;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import com.notes.notesplatform.model.Note;
 
 public interface NoteRepository extends JpaRepository<Note, Long> {
@@ -11,13 +14,26 @@ public interface NoteRepository extends JpaRepository<Note, Long> {
     List<Note> findByChapterIdAndDeletedFalse(Long chapterId);
    
 
-    @EntityGraph(attributePaths = {
-    "chapter",
-    "chapter.subject",
-    "chapter.subject.classEntity",
-    "chapter.subject.classEntity.course"
-})
 List<Note> findByDeletedTrue();
+
+   @Query("""
+SELECT new map(
+    n.id as id,
+    n.title as title,
+    n.fileUrl as fileUrl,
+    n.price as price,
+    n.isFree as isFree,
+    CONCAT(c.name, ' > ', ce.name, ' > ', s.name, ' > ', ch.name, ' > ', n.title) as fullPath
+)
+FROM Note n
+LEFT JOIN n.chapter ch
+LEFT JOIN ch.subject s
+LEFT JOIN s.classEntity ce
+LEFT JOIN ce.course c
+WHERE n.deleted = true
+""")
+List<Map<String, Object>> findDeletedNotesWithFullPath();
+
 
     
 }
